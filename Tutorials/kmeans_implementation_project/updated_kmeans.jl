e3e49ce48f4238f8d3acb01ff79b4332bc3ffe4f
Bernard Brenyah <pimpfada@gmail.com>
2020-02-09 21:52:34 +0100

    Create updated

---
+++ Tutorials/kmeans_implementation_project/updated_kmeans.jl
@@ -0,0 +1,25 @@
+# Replace python environment to suit your needs
+ENV["PYTHON"] = "/Users/mysterio/miniconda3/envs/pydata/bin/python"
+Pkg.build("PyCall")  # Build PyCall to suit the specified Python env
+
+using PyCall
+using Statistics
+using LinearAlgebra
+using Plots
+using BenchmarkTools
+
+
+"""
+"""
+function Kmeans(X::Array, k::Int64; max_iters::Int64=300, tol=1e-5)
+    # randomly get centroids for each group
+    n_row, n_col = size(design_matrix)
+    rand_indices = rand(1:n_row, k)
+    centroids = design_matrix[rand_indices, :]
+
+    # Update centroids & labels with closest members until convergence
+    for iter = 1:max_iters
+        
+    end
+    
+end
